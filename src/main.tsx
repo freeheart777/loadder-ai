bc6c453801b34893e4bdda1c8fa6e6c821b9984f
Mohammadreza Azaddel <Azaddel.777@gmail.com>
2026-09-05 01:36:39 +0330

    Load direct inline media interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./store-builder-mobile.css";
+import "./direct-media.css";
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
