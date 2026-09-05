c53f75239f86fcf0acfa4f61a2ea571b02fe4b4b
Mohammadreza Azaddel <Azaddel.777@gmail.com>
2026-09-05 23:38:29 +0330

    fix(crm): mark drag event import as type-only

--- src/pages/CRMSalesPipelinePage.tsx
+++ src/pages/CRMSalesPipelinePage.tsx
@@ -1,4 +1,4 @@
-import { DragEvent, useEffect, useMemo, useState } from "react";
+import { type DragEvent, useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   ArrowRight,
